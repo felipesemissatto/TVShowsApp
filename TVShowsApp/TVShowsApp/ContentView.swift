3138161ed72b4c0c042f12012e9d7947af9f6688
Felipe Semissatto <felipesemi@gmail.com>
2024-01-14 14:54:26 -0300

    Initial project

---
+++ TVShowsApp/TVShowsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TVShowsApp
+//
+//  Created by Felipe Semissatto on 14/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TVShowsApp/TVShowsApp/TVShowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowsAppApp.swift
+//  TVShowsApp
+//
+//  Created by Felipe Semissatto on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TVShowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

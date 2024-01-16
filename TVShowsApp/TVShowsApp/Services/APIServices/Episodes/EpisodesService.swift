165fe21c0864c83fdfcdf341dee702cd740e5f54
Felipe Semissatto <felipesemi@gmail.com>
2024-01-15 21:15:57 -0300

    Implemented Episodes service to fetch for episodes by the TV show id

---
+++ TVShowsApp/TVShowsApp/Services/APIServices/Episodes/EpisodesService.swift
@@ -0,0 +1,34 @@
+//
+//  EpisodesService.swift
+//  TVShowsApp
+//
+//  Created by Felipe Semissatto on 15/01/24.
+//
+
+import Foundation
+
+class EpisodesService: EpisodesServiceProtocol {
+    func getEpisodes(by tvShowId: Int, completion: @escaping (Result<[Episode], ApiServiceErrors>) -> Void) {
+        guard let url = URL(string: Endpoints.rootUrl+"/shows/\(tvShowId)/episodes") else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard let jsonData = data else {
+                completion(.failure(.noDataFound))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let decoded = try decoder.decode([Episode].self, from: jsonData)
+                completion(.success(decoded))
+            } catch {
+                completion(.failure(.failedToDecode))
+            }
+        }
+        task.resume()
+    }
+}
